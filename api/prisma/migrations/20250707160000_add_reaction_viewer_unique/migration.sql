f2ab3ae31fc9132911768b03a4dd1eb190578e8b
k1racle <alexmaltsev.am@gmail.com>
2026-07-08 10:05:57 +0300

    4.8.9

--- api/prisma/migrations/20250707160000_add_reaction_viewer_unique/migration.sql
+++ api/prisma/migrations/20250707160000_add_reaction_viewer_unique/migration.sql
@@ -1,2 +1,5 @@
+-- AddColumn
+ALTER TABLE "reactions" ADD COLUMN IF NOT EXISTS "viewer_id" TEXT;
+
 -- CreateIndex
 CREATE UNIQUE INDEX IF NOT EXISTS "reactions_contentItemId_viewer_id_key" ON "reactions"("contentItemId", "viewer_id");
