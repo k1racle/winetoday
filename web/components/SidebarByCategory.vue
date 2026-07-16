914cfc66f52c91cb910cfbe381c0195e4d9b13a7
k1racle <alexmaltsev.am@gmail.com>
2026-07-16 15:29:41 +0300

    2.1.9

--- web/components/SidebarByCategory.vue
+++ web/components/SidebarByCategory.vue
@@ -65,7 +65,7 @@ function truncatedTitle(title: string, hasLabel: boolean): string {
                 </span>
               </div>
               <div class="min-w-0 flex-1">
-                <p class="text-sm font-normal leading-snug text-foreground group-hover:text-foreground line-clamp-3" :class="{ 'font-bold': item.materialLabel === 'important' }">
+                <p class="text-sm font-normal leading-snug text-foreground transition-colors group-hover:text-accent line-clamp-3" :class="{ 'font-bold': item.materialLabel === 'important' }">
                   {{ truncatedTitle(item.title, !!item.materialLabel) }}
                   <MaterialLabelBadge
                     :label="item.materialLabel"
